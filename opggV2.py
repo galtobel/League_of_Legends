cb629ee674f7a6023d8e29766653eac175acfa30
Giuliano Altobelli <44181597+galtobel@users.noreply.github.com>
2018-11-13 01:32:25 -0800

    Update opggV2.py

--- opggV2.py
+++ opggV2.py
@@ -4,8 +4,7 @@ import pandas as pd
 import re
 import urllib.request
 from bs4 import BeautifulSoup
-import os
-from string import ascii_lowercase
+#import os
 
 def user():
     region_input = input("Welcome! This is a simple script that scrapes op.gg for the top 2000 players across each region. All you need to do is input what region you will like (NA,KR,JP,EUW,EUNE,OCE,BR,LAS,LAN,RU,TR)")
@@ -59,6 +58,8 @@ def make_soup(get_url):
     #print(get_rank[0])
     return get_data(get_rank,get_summoner_name,get_tier,get_LP,get_wr)
 
+#Global Variable
+resultset = {'rank_num' : [], 'summoner_name' : [], 'tier' : [], 'current_LP' : [], 'win_rate' : []}
 def get_data(get_rank,get_summoner_name,get_tier,get_LP,get_wr):
     """We could take this a step further once OP.GG fixes a few bugs. Right now this only works on the NA server but next patch should fix bugs when loading the page"""
     
@@ -69,20 +70,25 @@ def get_data(get_rank,get_summoner_name,get_tier,get_LP,get_wr):
         tier_list.append(re.sub('\s+','',tier.text)) # re.sub might be a bit difficult to read but I am still testing with regex once I release the full version. This is just getting rid delimiters in the text.
         lp_list.append(re.sub('\s+','',lp.text))
         wr_list.append(re.sub('\s+','',wr.div.span.text))
-    #print(rank_list)
-    return create_dataframe(rank_list, summoner_name_list, tier_list, lp_list, wr_list)
-
-def create_dataframe(rank_list, summoner_name_list, tier_list, lp_list, wr_list): 
+    resultset['rank_num'] += rank_list
+    resultset['summoner_name'] += summoner_name_list
+    resultset['tier'] += tier_list
+    resultset['current_LP'] += lp_list
+    resultset['win_rate'] += wr_list
+    create_dataframe(resultset)
+    
+def create_dataframe(resultset): 
     """This gets all of our data that we scraped and stores into a pandas dataframe"""
-    df = pd.DataFrame({'rank_num' : rank_list, 'summoner_name' : summoner_name_list, 'tier' : tier_list, 'current_LP' : lp_list, 'win_rate' : wr_list})
+    df = pd.DataFrame(data=resultset)
     df = df.reset_index(drop=True)
     #print(df)
     #os.chdir(r"C:/Users/Root/Desktop/LOL")
-    #df.to_excel("LeagueTest.xlsx")
+    df.to_excel("LeagueTest.xlsx",index=False) # We can send our user input here and create logic statements and fully automate the process
+
 
 def url(post_url):
     """This gets our user input based on what region they want"""
-    pages = [str(i) for i in range(1,2)]
+    pages = [str(i) for i in range(1,3)] # EATS UP RAM!!! Please be aware
     for page in pages:
         get_url = post_url + page
         make_soup(get_url)
